a3824165fe12e8efa4a4f8e78066739a03b5bbfa
Vadim Sokolov <108888585+vadimcodes@users.noreply.github.com>
2022-11-28 16:08:23 +0300

    реализовано: страница Контакты, секция Контакты на Главной странице, Страница Магазина и прочие доработки

---
+++ src/js/components/maps.js
@@ -0,0 +1,33 @@
+function init (){
+  let map = new ymaps.Map('map', {
+      center:[56.10186131310316,47.25180185844421],
+      zoom: 17
+  });
+
+  let placemark = new ymaps.Placemark([56.10145356864081,47.25464499999998],{
+    balloonContentHeader: 'Агротех21',
+    balloonContentFooter: 'г. Чебоксары, ул. Пристанционная, д. 3',
+    iconCaption: 'Компания "Агротех21"',
+  },{
+      iconImageSize:[30, 30],
+      iconColor: '#27AE4D',
+
+      iconImageOffset:[0,0]
+  })
+
+  map.controls.remove('geolocationControl');
+  map.controls.remove('searchControl');
+  map.controls.remove('trafficControl');
+  map.controls.remove('typeSelector');
+  map.controls.remove('fullscreenControl');
+  map.controls.remove('zoomControl');
+  map.controls.remove('rulerControl');
+  map.behaviors.disable(['scrollZoom']);
+
+  map.geoObjects.add(placemark);
+  // placemark.balloon.open();
+}
+
+
+
+ymaps.ready(init);
