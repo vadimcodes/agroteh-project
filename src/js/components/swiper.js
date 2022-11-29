59996788cfa104d019091bdd68ac508f1e5f8382
Oleg <swigerd2l@gmail.com>
2022-11-29 12:15:48 +0300

    Merge branch 'main' into kireev

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,13 +1,52 @@
 import Swiper from "swiper";
 
+// слайдер под хэдером
+new Swiper('.banner__main', {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 7000,
+  },
 
-new Swiper('.list-products', {
+  navigation: {
+    nextEl: '.swiper-body__main .swiper-body__btn-next',
+    prevEl: '.swiper-body__main .swiper-body__btn-prev',
+  },
+});
+
+// слайдер товаров Актуальные предложения
+new Swiper('.products__slider-actual', {
   slidesPerView: 4,
   spaceBetween: 30,
   autoHeight: true,
+
+  navigation: {
+    nextEl: '.swiper-body__actual .swiper-body__btn-next',
+    prevEl: '.swiper-body__actual .swiper-body__btn-prev',
+  },
 });
 
-new Swiper('.banner__swiper', {
+// слайдер на главной странице
+new Swiper('.banner__second', {
   slidesPerView: 1,
+  loop: true,
+
+
+  navigation: {
+    nextEl: '.swiper-body__second .swiper-body__btn-next',
+    prevEl: '.swiper-body__second .swiper-body__btn-prev',
+  },
+});
+
+// слайдер товаров Новинки
+new Swiper('.products__slider-new', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoHeight: true,
+
+  navigation: {
+    nextEl: '.swiper-body__new .swiper-body__btn-next',
+    prevEl: '.swiper-body__new .swiper-body__btn-prev',
+  },
 });
 
